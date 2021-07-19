874c8c27d316b66851cbf1e2e27c1fd96ddbda60
tomasz <tomekpdevelopment@gmail.com>
2021-07-19 13:36:57 +0200

    add DTL filters and tags (for loop) in html files

--- challenges/views.py
+++ challenges/views.py
@@ -22,16 +22,10 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -50,7 +44,7 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
         return render(request, "challenges/challenge.html", {
             "text": challenge_text,
-            "month": month.capitalize()
+            "month_name": month
         })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
